4b0d02b4bde7a99dfad11e272702e19d1e80e627
augorbunov <andrey.laponov@gmail.com>
2022-11-20 14:11:50 +0300

    fix tests after review and additional lesson

--- src/test/java/com/demoqa/AutomationPracticeFormTests.java
+++ src/test/java/com/demoqa/AutomationPracticeFormTests.java
@@ -4,9 +4,12 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTests {
     @BeforeAll
@@ -19,20 +22,59 @@ public class AutomationPracticeFormTests {
     @Test
     void fillFormTest () {
         open("/automation-practice-form");
+        //check that the page has been loaded
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        //remove useless elements from the page
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
 
+        //filling values
         $("#firstName").setValue("Name");
         $("#lastName").setValue("Surname");
         $("#userEmail").setValue("test@email.com");
+        //click radio button
+        //$("#gender-radio-1").parent().click();
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("1234567890");
 
-        $("#firstName").equals("Name");
-        $("#lastName").equals("Surname");
-        $("#userEmail").equals("test@email.com");
+        //input date of birth
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__day--029:not(react-datepicker__day--outside-month)").click();
+
+        //input subjects
+        $("#subjectsInput").setValue("Math").pressEnter();
+        //$("#subjectsInput").sendKeys("Maths");
+        //$("#subjectsInput").pressEnter();
+
+        //hobbies
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+
+        //upload picture
+        $("#uploadPicture").uploadFromClasspath("img/1.jpg");
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/img/1.jpg"));
+
+        $("#currentAddress").setValue("Address st. 1 apt.123");
+
+        //select state and city
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+
+        //submit form
+        $("#submit").click();
+
+
+        //check filled values
+        $(".modal-dialog").should(appear);
+
+        $(".table-responsive").shouldHave(text("Name"),text("Surname"),
+                text("Maths"),text("Karnal"));
+        //check a separate row
+        $(".table-responsive").$(byText("Address"))
+                .parent().shouldHave(text("Address st. 1 apt.123"));
     }
 
-    /*@Test
-    void fillWrongEmailTest () {
-        open("/automation-practice-form");
-
-        $("#userEmail").setValue("testemail.com");
-    }*/
 }
