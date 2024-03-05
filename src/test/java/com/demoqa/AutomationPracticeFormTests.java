f48566508f25e89888f15e87354dad008d11552a
augorbunov <andrey.laponov@gmail.com>
2024-03-05 09:57:48 +0300

    added checks for each row in output table

--- src/test/java/com/demoqa/AutomationPracticeFormTests.java
+++ src/test/java/com/demoqa/AutomationPracticeFormTests.java
@@ -71,11 +71,32 @@ public class AutomationPracticeFormTests {
         //check filled values
         $(".modal-dialog").should(appear);
 
-        $(".table-responsive").shouldHave(text("Name"),text("Surname"),
-                text("Maths"),text("Karnal"));
-        //check a separate row
+        //$(".table-responsive").shouldHave(text("Name"),text("Surname"),
+               // text("Maths"),text("Karnal"));
+
+        //check a separate rows
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text("Name Surname"));
+        $(".table-responsive").$(byText("Student Email"))
+                .parent().shouldHave(text("test@email.com"));
+        $(".table-responsive").$(byText("Gender"))
+                .parent().shouldHave(text("Other"));
+        $(".table-responsive").$(byText("Mobile"))
+                .parent().shouldHave(text("1234567890"));
+        $(".table-responsive").$(byText("Date of Birth"))
+                .parent().shouldHave(text("29 May,1990"));
+        $(".table-responsive").$(byText("Subjects"))
+                .parent().shouldHave(text("Maths"));
+        $(".table-responsive").$(byText("Hobbies"))
+                .parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture"))
+                .parent().shouldHave(text("1.jpg"));
         $(".table-responsive").$(byText("Address"))
                 .parent().shouldHave(text("Address st. 1 apt.123"));
+        $(".table-responsive").$(byText("State and City"))
+                .parent().shouldHave(text("Haryana Karnal"));
+
+
     }
 
 }
