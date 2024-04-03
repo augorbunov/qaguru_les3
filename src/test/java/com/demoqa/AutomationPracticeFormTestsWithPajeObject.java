745e9526eff8f057cda4fb378e22dfb12a2b65ca
augorbunov <andrey.laponov@gmail.com>
2024-04-03 09:21:31 +0300

    add new tests with page object

--- src/test/java/com/demoqa/AutomationPracticeFormTestsWithPajeObject.java
+++ src/test/java/com/demoqa/AutomationPracticeFormTestsWithPajeObject.java
@@ -3,11 +3,7 @@ package com.demoqa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import pages.RegistrationPage;
 
 public class AutomationPracticeFormTestsWithPajeObject {
     @BeforeAll
@@ -18,83 +14,60 @@ public class AutomationPracticeFormTestsWithPajeObject {
         Configuration.pageLoadStrategy = "eager";
     }
 
+    RegistrationPage registrationPage = new RegistrationPage();
     @Test
-    void fillFormTest () {
-        open("/automation-practice-form");
-        //check that the page has been loaded
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        //remove useless elements from the page
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#fixedban').remove()");
+    //test filling all fields on the form
+    void fillFullFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Name")
+                .setLastName("Surname")
+                .setEmail("test@email.com")
+                .setGender("Other")
+                .setUserNumber("1234567890")
+                .setDateOfBirth("29", "May", "1990")
+                .setSubjects(new String[]{"Math", "Biology"})
+                .setHobbies("Sports")
+                .uploadPicture("img/1.jpg")
+                .setAddress("Address st. 1 apt.123")
+                .setStateAndCity("Haryana", "Karnal")
+                .submitForm();
 
-        //filling values
-        $("#firstName").setValue("Name");
-        $("#lastName").setValue("Surname");
-        $("#userEmail").setValue("test@email.com");
-        //click radio button
-        //$("#gender-radio-1").parent().click();
-        $("#genterWrapper").$(byText("Other")).click();
-        $("#userNumber").setValue("1234567890");
-
-        //input date of birth
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("June");
-        $(".react-datepicker__year-select").selectOption("1990");
-        $(".react-datepicker__day--029:not(react-datepicker__day--outside-month)").click();
-
-        //input subjects
-        $("#subjectsInput").setValue("Math").pressEnter();
-        //$("#subjectsInput").sendKeys("Maths");
-        //$("#subjectsInput").pressEnter();
-
-        //hobbies
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-
-        //upload picture
-        $("#uploadPicture").uploadFromClasspath("img/1.jpg");
-        //$("#uploadPicture").uploadFile(new File("src/test/resources/img/1.jpg"));
-
-        $("#currentAddress").setValue("Address st. 1 apt.123");
-
-        //select state and city
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("Haryana")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Karnal")).click();
-
-        //submit form
-        $("#submit").click();
-
-
-        //check filled values
-        $(".modal-dialog").should(appear);
-
-        //$(".table-responsive").shouldHave(text("Name"),text("Surname"),
-               // text("Maths"),text("Karnal"));
-
-        //check separate rows
-        $(".table-responsive").$(byText("Student Name"))
-                .parent().shouldHave(text("Name Surname"));
-        $(".table-responsive").$(byText("Student Email"))
-                .parent().shouldHave(text("test@email.com"));
-        $(".table-responsive").$(byText("Gender"))
-                .parent().shouldHave(text("Other"));
-        $(".table-responsive").$(byText("Mobile"))
-                .parent().shouldHave(text("1234567890"));
-        $(".table-responsive").$(byText("Date of Birth"))
-                .parent().shouldHave(text("29 May,1990"));
-        $(".table-responsive").$(byText("Subjects"))
-                .parent().shouldHave(text("Maths"));
-        $(".table-responsive").$(byText("Hobbies"))
-                .parent().shouldHave(text("Sports"));
-        $(".table-responsive").$(byText("Picture"))
-                .parent().shouldHave(text("1.jpg"));
-        $(".table-responsive").$(byText("Address"))
-                .parent().shouldHave(text("Address st. 1 apt.123"));
-        $(".table-responsive").$(byText("State and City"))
-                .parent().shouldHave(text("Haryana Karnal"));
-
+        //check results
+        registrationPage.checkResultPositive()
+                .checkResult("Student Name", "Name Surname")
+                .checkResult("Student Email", "test@email.com")
+                .checkResult("Gender", "Other")
+                .checkResult("Mobile", "1234567890")
+                .checkResult("Date of Birth", "29 May,1990");
 
     }
 
+    @Test
+    //test filling only required fields
+    void fillRequiredFieldsFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Name")
+                .setLastName("Surname")
+                .setGender("Other")
+                .setUserNumber("1234567890")
+                .submitForm();
+
+        //check results
+        registrationPage.checkResultPositive()
+                .checkResult("Student Name", "Name Surname")
+                .checkResult("Gender", "Other")
+                .checkResult("Mobile", "1234567890");
+    }
+
+    @Test
+    //test negative asserts
+    void negativeFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Name")
+                .setLastName("Surname")
+                .submitForm();
+
+        //check results
+        registrationPage.checkResultNegative();
+    }
 }
---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,133 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+    private SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            userSubjectsInput = $("#subjectsInput"),
+            userHobbiesInput = $("#hobbiesWrapper"),
+            calendarInput = $("#dateOfBirthInput"),
+            pictureInput = $("#uploadPicture"),
+            addressInput = $("#currentAddress"),
+            stateInput = $("#state"),
+            cityInput = $("#city"),
+            cityStateWrapper = $("#stateCity-wrapper"),
+            submitButton = $("#submit"),
+
+            resultHeader = $("[class]");;
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationPage setSubjects(String[] strArray) {
+        for (String str : strArray) {
+            userSubjectsInput.setValue(str).pressEnter();
+        }
+
+        return this;
+    }
+    public RegistrationPage setHobbies(String value) {
+        userHobbiesInput.$(byText(value)).click();
+
+        return this;
+    }
+    public RegistrationPage uploadPicture(String value) {
+        pictureInput.uploadFromClasspath(value);
+
+        return this;
+    }
+
+    public RegistrationPage setAddress(String value) {
+        addressInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setStateAndCity(String state, String city) {
+        stateInput.click();
+        cityStateWrapper.$(byText(state)).click();
+        cityInput.click();
+        cityStateWrapper.$(byText(city)).click();
+
+        return this;
+    }
+
+    public RegistrationPage submitForm() {
+        submitButton.click();
+
+        return this;
+    }
+
+    public RegistrationPage checkResult(String key, String value) {
+        $(".table-responsive").$(byText(key)).parent()
+                .shouldHave(text(value));
+
+        return this;
+    }
+
+    public RegistrationPage checkResultPositive() {
+        resultHeader.shouldHave((text("Thanks for submitting the form")));
+
+        return this;
+    }
+    public RegistrationPage checkResultNegative() {
+        resultHeader.shouldNotHave((text("Thanks for submitting the form")));
+
+        return this;
+    }
+}
